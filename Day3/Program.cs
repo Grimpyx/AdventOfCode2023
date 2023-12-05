f8d74024cb5ae2efb5586b08b9d50e95e97b351f
Jesper F <jesperfagerberg@live.se>
2023-12-05 14:49:44 +0100

    Commenting and cleaning up Day 3

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -2,51 +2,34 @@
 
 Schematic schematic = new Schematic(File.ReadAllLines("./data_complete.txt"));
 
-
-// Program start
-for (int qqq = 0; qqq < 500; qqq++)
+/*
+// loop through schematic and write it
+// The large schematic is way too big for this
+for (int x = 0; x < schematic.Dimension[0]; x++) // x-value from top left
 {
-    /*
-    // loop through schematic and write it
-    for (int x = 0; x < schematic.Dimension[0]; x++) // x-value from top left
+    for (int y = 0; y < schematic.Dimension[1]; y++) // y value from top left
     {
-        for (int y = 0; y < schematic.Dimension[1]; y++) // y value from top left
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(schematic.Symbols[x, y]);
-        }
-    }*/
-
-    Console.WriteLine("\n >> X: {0}   Y: {1}", schematic.Dimension[0], schematic.Dimension[1]);
-    Console.WriteLine("\n >> [x,y]=[1,4]: {0}", schematic.Symbols[1, 4]);
-
-    /*string? input = Console.ReadLine();
-    if (int.TryParse(input, out int inputNumber))
-    {
-        Console.WriteLine(" Row: " + schematic.GetColumn(inputNumber));
-        Console.WriteLine(" Col: " + schematic.GetRow(inputNumber));
-    }*/
-    List<Schematic.Map> maps = schematic.FindAllMaps();
-    int sumOfAllPartNumbers = 0;
-    foreach (Schematic.Map map in maps)
-    {
-        if (map.HasSymbolAdjacent()) // only a part number if it has adjacent symbol
-            sumOfAllPartNumbers += map.Value;
+        Console.SetCursorPosition(x, y);
+        Console.Write(schematic.Symbols[x, y]);
     }
-    Console.WriteLine(" >>>>> Sum of parts: " + sumOfAllPartNumbers);
-    int sumOfRatios = 0;
-    foreach (int ratio in Schematic.Map.FindGearRatios(maps))
-        sumOfRatios += ratio;
-    Console.WriteLine(" >>>>> Sum of ratio: " + sumOfRatios);
+}*/
 
-
-
-    Console.Write("\n\nPress anything to continue...");
-    Console.Read();
-    Console.Clear();
-
-
-
-
-
+List<Schematic.Map> maps = schematic.FindAllMaps();
+int sumOfAllPartNumbers = 0;
+foreach (Schematic.Map map in maps)
+{
+    // All part numbers has a symbol adjacent.
+    if (map.HasSymbolAdjacent())
+        sumOfAllPartNumbers += map.Value;
 }
+Console.WriteLine(" >  Sum of parts: " + sumOfAllPartNumbers);
+int sumOfRatios = 0;
+foreach (int ratio in Schematic.Map.FindGearRatios(maps))
+    sumOfRatios += ratio;
+Console.WriteLine(" > Sum of ratios: " + sumOfRatios);
+
+
+
+Console.Write("\n\nPress anything to continue...");
+Console.Read();
+Console.Clear();
--- Day3/Schematic.cs
+++ Day3/Schematic.cs
@@ -42,7 +42,8 @@ namespace Day3
                     {
                         int stepsToJump = 0;
 
-                        while (char.IsDigit(row[x+stepsToJump]))//(char.IsDigit(row[Math.Clamp(x + stepsToJump, 0, Dimension[0]-1)]))
+                        // if current char is a digit
+                        while (char.IsDigit(row[x+stepsToJump]))
                         {
                             stepsToJump++;
                             if (x + stepsToJump >= Dimension[0]) break;
@@ -58,12 +59,7 @@ namespace Day3
                             Dimension);
                         returnMap.Add(map);
 
-                        // Create submap of symbols
-                        
-                        //if (map.HasSymbolAdjacent()) Console.WriteLine(map.GetPrintableString(Symbols, Dimension) + "    > Val: " + map.Value + "    steps: " + map.ValueCharacterLength);
-                        //Console.WriteLine("Has adjacent. >>>>>>>>>>>>>>>>\n");
-                        //else Console.WriteLine("\n");
-
+                        // Skip a few iterations of the loop by stepping past the digits
                         x += stepsToJump;
                     }
                 }
@@ -115,9 +111,7 @@ namespace Day3
                     }
                 }
 
-                //Console.WriteLine("Chars: " + allCharsAdjacent);
                 allCharsAdjacent = allCharsAdjacent.Trim(".0123456789".ToCharArray());
-                //Console.WriteLine("Chars: " + allCharsAdjacent);
 
                 if (allCharsAdjacent.Length > 0) return true;
                 else return false;
@@ -127,8 +121,8 @@ namespace Day3
             {
                 string returnString = "";
 
-                // We essentially grab all symbols around the number,
-                // but clamp the range so we dont go outside the schematic
+                // We essentially grab the number and all symbols around it,
+                // but clamp the range so we dont go outside the intended map (or the whole schematic)
                 int xLim_low = Math.Clamp(X_coordinate - 1, 0, dim[0]); 
                 int xLim_high = Math.Clamp(X_coordinate + ValueCharacterLength + 1, 0, dim[0]);
                 int yLim_low = Math.Clamp(RowNr - 1, 0, dim[1]);
@@ -145,10 +139,16 @@ namespace Day3
                 return returnString;
             }
 
+            /// <summary>
+            /// Creates the 2D array for all symbols around a the number
+            /// </summary>
+            /// <param name="sym">The whole schematic</param>
+            /// <param name="dim">dimensions of the schematic</param>
+            /// <returns></returns>
             private char[,] CreateSymbolMap(char[,] sym, int[] dim)
             {
-                // We essentially grab all symbols around the number,
-                // but clamp the range so we dont go outside the schematic
+                // We essentially grab the number and all symbols around it,
+                // but clamp the range so we dont go outside the intended map (or the whole schematic)
                 int xLim_low = Math.Clamp(X_coordinate - 1, 0, dim[0]);
                 int xLim_high = Math.Clamp(X_coordinate + ValueCharacterLength+1, 0, dim[0]);
                 int yLim_low = Math.Clamp(RowNr - 1, 0, dim[1]);
@@ -158,11 +158,11 @@ namespace Day3
                 int symbols_x = 0; // indexes for the local field "symbols"
                 int symbols_y = 0;
 
-                // Row loop
+                // Row loop (y)
                 for (int y = yLim_low; y < yLim_high; y++)
                 {
                     symbols_x = 0;
-                    // x coordinate loop
+                    // x coordinate loop (x)
                     for (int x = xLim_low; x < xLim_high; x++)
                     {
                         returnMatrix[symbols_x, symbols_y] = sym[x, y];
@@ -170,6 +170,17 @@ namespace Day3
                     }
                     symbols_y++;
                 }
+
+                // loop through map and write it. Put breakpoint on this line to step through all maps.
+                /*for (int x = 0; x < returnMatrix.GetLength(0); x++) // x-value from top left
+                {
+                    for (int y = 0; y < returnMatrix.GetLength(1); y++) // y value from top left
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(returnMatrix[x, y]);
+                    }
+                }*/
+
                 return returnMatrix;
             }
 
@@ -180,60 +191,82 @@ namespace Day3
                 List<Map> gearContenders = new List<Map>();
                 List<int> gear_x = new List<int>();
                 List<int> gear_y = new List<int>();
+
+                // Loop through all maps
                 foreach (Map map in mapList)
                 {
-                    string allChars = "";
+                    // Loop through x-coordinate
                     for (int i = 0; i < map.symbols.GetLength(0); i++)
                     {
+                        // Loop through y-coordinate
                         for (int j = 0; j < map.symbols.GetLength(1); j++)
                         {
-                            allChars += map.symbols[i, j];
+                            // If it contains the symbol '*', it is a "gear contender".
+                            // Still needs to be adjacent to another gear contender. So we check that below:
                             if (map.symbols[i, j] == '*')
                             {
                                 gearContenders.Add(map);
 
-                                // check first row and first column for numbers. If numbers is found, +1
+                                // We will check if the first row and/or column contains a number.
+                                // If it does, it means we need to shift the coordinate reference down and/or right,
+                                // because we assume most numbers start in the middle of the map, not the beginning
+                                //
+                                //  For example: .644. needs to be shifted downward one step (y++)
+                                //               *....
+                                //
+                                //  and this      53.  needs to be shifted down and right (x++, y++)
+                                //                .*.
+                                //
                                 int global_x = map.X_coordinate; // start at global x-value of the first number
                                 int global_y = map.RowNr;        // start at global y-value of the first number
                                 global_x += -1; // we move 1 step to the left of the first number offset for our local coordinate
                                 global_y += -1; // we move 1 step above the first number offset for our local coordinate
-                                global_x += i;
-                                global_y += j;
-                                for (int k = 0; k < map.symbols.GetLength(1); k++)
+                                global_x += i;  // add our local coordinate (coordinate of the loop [i,j] where we found a '*'
+                                global_y += j;  // add our local coordinate (coordinate of the loop [i,j] where we found a '*'
+                                for (int k = 0; k < map.symbols.GetLength(1); k++) // first column
                                 {
+                                    // Counterintuitively, the value of x is actually the column number.
+                                    // In map.symbols[0, k] we loop through the first col
                                     if (char.IsDigit(map.symbols[0, k]))
                                     {
-                                        Console.WriteLine(map.Value + " X:" + map.symbols[0, k]);
                                         global_x++; // adjust coordinate system right one step if we an edge case scenario
                                         break;
                                     }
                                 }
-                                for (int k = 0; k < map.symbols.GetLength(0); k++)
+                                for (int k = 0; k < map.symbols.GetLength(0); k++) // first row
                                 {
+                                    // Counterintuitively, the value of y is actually the row number.
+                                    // In map.symbols[k, 0] we loop through the first row
                                     if (char.IsDigit(map.symbols[k, 0]))
                                     {
-                                        Console.WriteLine(map.Value + " Y:" + map.symbols[k, 0]);
                                         global_y++; // adjust coordinate system down one step if we an edge case scenario
                                         break;
                                     }
                                 }
 
                                 gear_x.Add(global_x); gear_y.Add(global_y);
-                                Console.WriteLine("{0} [{1},{2}]", map.Value, global_x, global_y);
-                                //Console.WriteLine(" - " + map.Value + " might be part of a gear at [{0},{1}]", global_x, global_y);
+                                //Console.WriteLine("{0} [{1},{2}]", map.Value, global_x, global_y);
                             }
                         }
                     }
                 }
 
-                for (int i = 0; i < gearContenders.Count; i++)      // gearContenders[i] is the current one
+                // Compare all gear contenders with eachother, to see if they share '*'
+                for (int i = 0; i < gearContenders.Count; i++)
                 {
-                    for (int j = i+1; j < gearContenders.Count; j++)  // gearContenders[j] comparing   // we set j = i because we dont want to compare to what came before
+
+                    // gearContenders[i] is the current one
+                    // gearContenders[j] the one we're comparing to comparing.
+                    // We set j = i because we dont want to compare to what came before.
+                    // This makes it such that we don't compare two numbers twice (for example map1 == map2,  map2 == map1).
+                    // we also add +1 cause we don't want to compare with itself (cause they will always return true: map1 == map1)
+                    for (int j = i+1; j < gearContenders.Count; j++)
                     {
+                        // If their '*' respective coordinates match
                         if (gear_x[i] == gear_x[j] && gear_y[i] == gear_y[j])
                         {
                             gearRatios.Add(gearContenders[i].Value * gearContenders[j].Value);
-                            Console.WriteLine("Found ratio between {0} and {1}: {2}", gearContenders[i].Value, gearContenders[j].Value, gearContenders[i].Value * gearContenders[j].Value);
+                            //Console.WriteLine("Found ratio between {0} and {1}: {2}", gearContenders[i].Value, gearContenders[j].Value, gearContenders[i].Value * gearContenders[j].Value);
                         }
                     }
                 }
