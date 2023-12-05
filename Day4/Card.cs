fea9fff7fb3ce91d18c42ce8f18c99a06bd34408
Jesper F <jesperfagerberg@live.se>
2023-12-05 16:52:04 +0100

    Day4 part 1

---
+++ Day4/Card.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    public class Card
+    {
+        public int id;
+        public int[] winningNumbers;
+        public int[] numbersYouHaveNumbers;
+
+        public int totalNumberOfWins = 0;
+
+        public Card(int id, int[] winningNumbers, int[] numbersYouHaveNumbers)
+        {
+            this.id = id;
+            this.winningNumbers = winningNumbers;
+            this.numbersYouHaveNumbers = numbersYouHaveNumbers;
+
+            CalculateNumberOfWins();
+        }
+
+        public Card(string lineInput)
+        {
+            string[] split = lineInput.Split(": ");
+
+            // split[0]             Take substring "Card   4"
+            // ["Card".Length..]    Ignore all characters 
+            // .TrimStart()         Remove all white spaces
+            this.id = int.Parse(split[0]["Card".Length..].TrimStart());
+            split = split[1].Split(" | "); // <-- [0] and [1] the two lists of ints
+
+            this.winningNumbers = ParseNumbers(split[0].Split(' '));
+            this.numbersYouHaveNumbers = ParseNumbers(split[1].Split(' '));
+
+            CalculateNumberOfWins();
+        }
+
+        private void CalculateNumberOfWins()
+        {
+            // Calculate total number of wins
+            foreach (int nrYouHave in this.numbersYouHaveNumbers)
+            {
+                foreach (int winNr in this.winningNumbers)
+                {
+                    if (nrYouHave == winNr)
+                    {
+                        totalNumberOfWins++;
+                    }
+                }
+            }
+        }
+
+        private int[] ParseNumbers(string[] numbersAsString)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < numbersAsString.Length; i++)
+            {
+                if (numbersAsString[i].Trim() == string.Empty) continue;
+                numbers.Add(int.Parse(numbersAsString[i].Trim())); // Trim() to remove white spaces in front and afterward
+            }
+            return numbers.ToArray();
+        }
+
+        public override string? ToString()
+        {
+            string returnString = $"Card {this.id}: ";
+            foreach (int number in this.winningNumbers) { returnString += number + " "; }
+            returnString += "| ";
+            foreach (int number in this.numbersYouHaveNumbers) { returnString += number + " "; }
+            return returnString;
+        }
+    }
+}
--- Day4/Program.cs
+++ Day4/Program.cs
@@ -6,7 +6,72 @@ namespace Day4
     {
         private static void Main(string[] args)
         {
-
+            //PartOne();
+            PartTwo();
         }
+
+        private static void PartOne()
+        {
+            string[] data = File.ReadAllLines("./data_complete.txt");
+
+            List<Card> cards = new List<Card>();
+            foreach (string s in data)
+            {
+                cards.Add(new Card(s));
+            }
+
+            int totalPoints = 0;
+            foreach (Card card in cards)
+            {
+                // Determine points for the card
+                int points = 0;
+                foreach (int nrYouHave in card.numbersYouHaveNumbers)
+                {
+                    foreach (int winNr in card.winningNumbers)
+                    {
+                        if (nrYouHave == winNr)
+                        {
+                            points *= 2;
+                            if (points == 0) points = 1;
+                        }
+                    }
+                }
+                totalPoints += points;
+
+                Console.WriteLine(card.ToString() + "  > Points: " + points);
+            }
+            Console.WriteLine(" >>> Total points: " + totalPoints);
+        }
+
+        private static void PartTwo()
+        {
+            string[] data = File.ReadAllLines("./data_test.txt");
+
+            List<Card> initialCards = new List<Card>();
+            foreach (string s in data)
+            {
+                initialCards.Add(new Card(s));
+            }
+
+            int totalCards = initialCards.Count;
+
+            foreach (Card card in initialCards)
+            {
+                Console.WriteLine(" " + card);
+                List<Card> additionalCards = new List<Card>();
+                for (int i = 0; i < card.totalNumberOfWins; i++)
+                {
+                    Card newCard = initialCards[card.id + i];
+                    Console.WriteLine(" > " + newCard);
+                    additionalCards.Add(newCard); // -1 cause range is 1,2,3,4 but we need index range 0,1,2,3
+                }
+                totalCards += additionalCards.Count;
+
+                //Console.WriteLine(card.ToString() + "  > Points: " + points);
+            }
+            Console.WriteLine(" >>> Total cards: " + totalCards);
+        }
+
+        private static List<Folder> ListFolders(Folder initialFolder)
     }
 }
