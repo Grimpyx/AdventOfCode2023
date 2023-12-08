f83eb7b6d6688e3cff303d277478ca046bbb22dc
Jesper Fagerberg <jesperfagerberg@live.se>
2023-12-09 00:04:51 +0100

    Added Day 6

---
+++ Day6/DataInterpret.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    public static class DataInterpret
+    {
+        public static int[] GetNumbersFrom(string sample, char separationChar)
+        {
+            List<int> numbers = new List<int>();
+
+            // Prune all characters except numbers and the separationChar
+            // String should not contain anything except numbers and separationChar after this
+            List<char> sampleAsList = new List<char>();
+            for (int i = 0; i < sample.Length; i++)
+            {
+                char currentChar = sample[i];
+                //Console.WriteLine("CurrentChar: " + currentChar);
+                if (currentChar == separationChar || char.IsDigit(currentChar))
+                    sampleAsList.Add(currentChar);
+            }
+            string prunedSample = new string(sampleAsList.ToArray());
+
+
+            // Separate into distinct strings with only numbers, and then parse them into integers
+            string[] numbersAsString = prunedSample.Split(separationChar);
+            for (int i = 0; i < numbersAsString.Length; i++)
+            {
+                if (numbersAsString[i] == string.Empty) continue; // skips empty strings
+                int nr = int.Parse(numbersAsString[i]);
+                numbers.Add(nr);
+            }
+
+            // This is a so called collection expression.
+            // Creates array from all "IEnumerable entries" in numbers
+            return [.. numbers]; 
+        }
+    }
+}
---
+++ Day6/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Day6;
+
+Console.WriteLine("Hello, World!");
+
+string[] data = File.ReadAllLines("./data_test.txt");
+DataInterpret.GetNumbersFrom(data[0], ' ');
