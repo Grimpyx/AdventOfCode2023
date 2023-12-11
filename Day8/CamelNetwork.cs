732829b51b852a9609a5d19035555a5ca577c9b6
Jesper F <jesperfagerberg@live.se>
2023-12-11 12:15:25 +0100

    Completed day 8 part 1

--- Day8/CamelNetwork.cs
+++ Day8/CamelNetwork.cs
@@ -4,7 +4,11 @@ namespace Day8
 {
     public class CamelNetwork
     {
-        public enum Instruction;
+        public enum Instruction
+        {
+            Left = 'L',
+            Right = 'R'
+        }
         private Instruction[] instructions;
 
         private Dictionary<string, CamelNext> nodes = new Dictionary<string, CamelNext>();
@@ -20,10 +24,53 @@ namespace Day8
             // Interpret data
             // The first row is all the instructions
             string instructionsAsString = readAllLinesInput[0];
+            instructions = new Instruction[instructionsAsString.Length];
+            for (int i = 0; i < instructionsAsString.Length; i++)
+                instructions[i] = (Instruction)instructionsAsString[i];
 
 
             // The first row is all the instructions
             string[] nodesAsStrings = readAllLinesInput[2..];
+            for (int row = 0; row < nodesAsStrings.Length; row++)
+            {
+                string from = nodesAsStrings[row].Split(" = ")[0];
+                string[] rightHand = nodesAsStrings[row].Split(", ");
+                string toLeft = rightHand[0][(rightHand[0].Length-3)..];
+                string toRight = rightHand[1][..3];
+
+                nodes.Add(from, new CamelNext(toLeft, toRight));
+            }
+        }
+
+        public int FindStepsToReach(string start, string destination)
+        {
+            int steps = 0;
+            int currentInstructionIndex = 0;
+
+            string current = start;
+
+            // loop through instructions
+            while (current != destination)
+            {
+                if (currentInstructionIndex >= instructions.Length) currentInstructionIndex = 0;
+
+                switch (instructions[currentInstructionIndex])
+                {
+                    case Instruction.Left:
+                        current = nodes[current].Left;
+                        break;
+                    case Instruction.Right:
+                        current = nodes[current].Right;
+                        break;
+                    default:
+                        break;
+                }
+
+                currentInstructionIndex++;
+                steps++;
+            }
+
+            return steps;
         }
 
         public struct CamelNext
@@ -38,10 +85,5 @@ namespace Day8
             public string Right { get; init; }
         }
 
-        public int FindEnd()
-        {
-            int steps = 0;
-            return steps;
-        }
     }
 }
--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,1 +1,13 @@
 ﻿
+using Day8;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+// Answers P1:
+// 457 is too low
+// 19951 was correct! I had not set start/destination to AAA->ZZZ (it was the start of the keys, and end of keys)
+
+// Answers P2:
+
+CamelNetwork net = new CamelNetwork(File.ReadAllLines("./data_complete.txt"));
+Console.WriteLine(" > Steps to complete: " + net.FindStepsToReach("AAA", "ZZZ"));
