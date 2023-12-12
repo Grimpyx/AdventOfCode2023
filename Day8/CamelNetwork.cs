16f865373a0cb54a1b28d0e73c25707af398c497
Jesper F <jesperfagerberg@live.se>
2023-12-12 19:36:29 +0100

    Completed day 8 part 2

--- Day8/CamelNetwork.cs
+++ Day8/CamelNetwork.cs
@@ -1,4 +1,6 @@
-﻿using static Day8.CamelNetwork;
+﻿using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using static Day8.CamelNetwork;
 
 namespace Day8
 {
@@ -75,65 +77,202 @@ namespace Day8
 
         public long FindStepsToReachGhostMode()
         {
+            // I looked up some help online, and they talk about a least common multiple?
+            // There's apparently a pattern, that for each place you start, only ONCE do you stop by a node ending with Z.
+
+            // After thinking about it I think you could geometrically look at it this way.
+            // First we assume 
+            // Assume for any Start the iterations required to reach the end looks something like this
+            // -----O----   <-- The whole loop. "O" is the destinatione, "-" is any other node.
+            // For different starts, they have different lengths.
+            // For example:
+            // -----O       (length 6)
+            // ---O         (length 4)
+            // ----O        (length 5)
+            //
+            // If we loop through every single one you get something like this
+            // -----O-----O-----O-----O-----O-----O-----O-----O-----O-----O           ---O--   nr of loops = 4*5 = 20         total steps = 20 * 6 = 120
+            // ---O---O---O---O---O---O---O---O---O---O---O---O---O---O---O           -O--     nr of loops = 6*5 = 30         total steps = 30 * 4 = 120
+            // ----O----O----O----O----O----O----O----O----O----O----O----O           --O--    nr of loops = 6*4 = 24         total steps = 24 * 5 = 120
+            //                                                          | <-- HERE is where they coincide. The distance until this point is 57
+            //                                                                which happens to be the least common multiple
+            // 
+            // LCM method:
+            // 6 = 2*3  => NEEDS a 2 and 3
+            // 4 = 2*2  => NEEDS two 2
+            // 5 = 5    => NEEDS a 5        => 2*2*3*5 = 60
+
             string[] allNodesEndingWithA = nodes.Keys.Where(x => x.EndsWith('A')).ToArray();
-
-            long steps = 0;
-            int printCounter = 0;
-            int currentInstructionIndex = 0;
-
             string[] current = allNodesEndingWithA;
+            
+            List<long> listOfLoops = new List<long>();
+            foreach (string s in current)
+            {
+                (long, long) lengths = GetLoopLength(s);
+                listOfLoops.Add(lengths.Item1);
+                Console.WriteLine("Loop length of " + lengths.Item1 + " remainder " + lengths.Item2 + "\n");
+            }
+            long lcm = LeastCommonMultiple(listOfLoops.ToArray());
+            Console.WriteLine("\n > Least common multiple for all items: " + lcm);
 
-            // loop through instructions
-            bool continueCondition = true;
-            while (continueCondition)
+
+            return lcm;
+        }
+
+        // My own algorithm
+        private long LeastCommonMultipleNumeric(long[] list)
+        {
+            long leastCommonMultiple = -1;
+            long[] incrementedList = [..list];
+            while (true)
+            {
+                long maxVal = incrementedList.Max();
+
+                // Increase all except the largest number with 1 step
+                for (int i = 0; i < incrementedList.Length; i++)
+                {
+                    if (incrementedList[i] == maxVal) continue;
+                    else incrementedList[i] += list[i];
+                }
+                /*foreach (var item in incrementedList)
+                {
+                    Console.WriteLine(" - " + item);
+                }*/
+
+                // if item1 == item2 == ... == itemN, we have reached a common multiple
+                bool shouldIterate = false;
+                foreach (var item in incrementedList)
+                {
+                    shouldIterate |= (item != maxVal);
+                }
+
+                if (shouldIterate) continue;
+                else return maxVal;
+            }
+        }
+
+        // There is a faster method of calculating the least common multiple, by dividing the product of the list of values with the greatest common divisor
+        // One way to calc the GCD: https://en.wikipedia.org/wiki/Euclidean_algorithm#Worked_example <-- see the worked example
+        // Apparently, assuming X>Y, the GCD of X and Y is the same as for X and X-Y.
+        // Essentially you can recursively make X and Y smaller and smaller until you get a remainder of 0, meaning you found the least common divisor.
+        // If you have many values, the least common divisor for all values can be calculated doing  GCD(A,B ... Z) = GCD(A,GCD(B,GCD(C,D))) and on and on and on until you hit Z
+        // Below is my implementation of that.
+        public static long GreatestCommonDenominator(long[] list)
+        {
+            // can't find GCD for a list of length 1
+            if (list.Length < 2) return -1;
+
+            // If we have more than 2 values, we have to recursively call this until it is only 2 values.
+            if (list.Length > 2)
+            {
+                long gcdForTheFirstTwo = GreatestCommonDenominator(list[1..]);
+                list = [list[0], gcdForTheFirstTwo];
+            }
+
+            // If we have only 2 values we can proceed
+            if (list.Length == 2)
+            {
+                long greater = Math.Max(list[0], list[1]);
+                long lesser = Math.Min(list[0], list[1]);
+
+                while (true)
+                {
+                    //Console.WriteLine("OLD - Greater: " + greater + "   Lesser: " + lesser);
+                    long remainder = greater;
+
+                    // instead of doing -lesser over and over again we can just use modulus to get the remainder
+                    remainder = greater % lesser;
+                    if (remainder == 0) return lesser; // if true, it is the GCD
+
+                    // When out of that loop, assign new Greater and Lesser
+                    greater = lesser;
+                    lesser = remainder;
+                }
+            }
+
+            Console.WriteLine("How did you get here?");
+            return -1;
+        }
+        public static long LeastCommonMultiple(long[] list)
+        {
+            // can't find LCM for a list of length 1
+            if (list.Length < 2) return -1;
+
+            // If we have more than 2 values, we have to recursively call this until it is only 2 values.
+            if (list.Length > 2)
+            {
+                long lcmForTheFirstTwo = LeastCommonMultiple(list[1..]);
+                list = [list[0], lcmForTheFirstTwo];
+            }
+
+            // If we have only 2 values we can proceed
+            if (list.Length == 2)
+            {
+                // We follow the definition of lcm(A,B) = (A*B)/gcd(A,B) from https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
+                ulong product = 1;
+                foreach (long item in list) product *= (ulong)item;
+                ulong gcd = (ulong)GreatestCommonDenominator(list);
+                return (long)(product/gcd);
+            }
+
+            Console.WriteLine("How did you get here?");
+            return -1;
+        }
+
+
+        // if iterates over the camelnetwork to find a loop. If it does, it sends the length of it.
+        // The first long returned is the length, and the second is the offset between the loop end and the goal "reach anything ending with a Z"
+        // for example: ----Z--- has length 8 and an offset of -3 because the three spaces after the goal.
+        private (long, long) GetLoopLength(string nodeSource)
+        {
+            long currentInstructionIndex = 0;
+            long stepsUntilZ = 0;
+            long zOffsetFromLoopEnd = 0;
+
+            string currentSource = nodeSource;
+            string nextSource = "";
+            string zSource = "";
+
+            while (true)
             {
                 if (currentInstructionIndex >= instructions.Length) currentInstructionIndex = 0;
-                if (printCounter >= 2500000)
+                stepsUntilZ++;
+
+                // After following the instruction we store the node in nextSource
+                Instruction nextInstruction = instructions[currentInstructionIndex];
+                switch (nextInstruction)
                 {
-                    Console.WriteLine("steps: " + steps);
-                    printCounter = 0;
+                    case Instruction.Left:
+                        nextSource = nodes[currentSource].Left;
+                        break;
+                    case Instruction.Right:
+                        nextSource = nodes[currentSource].Right;
+                        break;
+                    default:
+                        break;
                 }
 
-                bool allEndsWithZ = true;
-                // Every index
-                for (int i = 0; i < current.Length; i++)
+                // if the next source is the same as the previous node ending in Z the we know for certain we've done a loop
+                if (nextSource == zSource)
                 {
-                    switch (instructions[currentInstructionIndex])
-                    {
-                        case Instruction.Left:
-                            current[i] = nodes[current[i]].Left;
-                            break;
-                        case Instruction.Right:
-                            current[i] = nodes[current[i]].Right;
-                            break;
-                        default:
-                            break;
-                    }
-
-                    if (current[i].EndsWith('Z')) allEndsWithZ &= true;
-                    else
-                    {
-                        allEndsWithZ &= false;
-                        break; // if this index fails on 1, all fail, so we exit the loop
-                    }
+                    break;
                 }
 
-                continueCondition = !allEndsWithZ;
-
+                // If the source ends with Z we start counting the loop length
+                // The value in counter stepsUntilZ was used to track the z offset.
+                if (currentSource.EndsWith('Z'))
+                {
+                    //Console.WriteLine("Steps until " + nextSource + ": " + stepsUntilZ);
+                    zSource = nextSource;
+                    zOffsetFromLoopEnd = stepsUntilZ;
+                    stepsUntilZ = 0;
+                }
+                currentSource = nextSource;
                 currentInstructionIndex++;
-                steps++;
-                printCounter++;
             }
-
-            Console.WriteLine($" - After {steps} steps, found the following values ending with Z: ");
-            Console.Write("     ");
-            foreach (string c in current)
-            {
-                Console.Write(c + "  ");
-            }
-            Console.WriteLine("\n");
-
-            return steps;
+            zOffsetFromLoopEnd -= stepsUntilZ;
+            //Console.WriteLine("Found duplicate at " + currentSource + " after " + totalCounter + " steps.");
+            return (stepsUntilZ, zOffsetFromLoopEnd);
         }
 
         public struct CamelNext
--- Day8/Program.cs
+++ Day8/Program.cs
@@ -6,7 +6,11 @@ using Day8;
 // 19951 was correct! I had not set start/destination to AAA->ZZZ (it was the start of the keys, and end of keys)
 
 // Answers P2:
+// 16342438708751 (from my numeric finder) (this calculator confirms http://www.alcula.com/calculators/math/lcm/)
+// 20906588438832807 (first result from my non numeric but it didnt work)
+// I had to let it be done recursively ((A*B)/lcm(A,B) didnt work when I first got the product of all factors and divided by their lcm, i.e. PRODUCT(A,B,..,Z)/lcm(A,B,..,Z) didnt work)
+// Now I got 16342438708751 just like I did from my brute force algorithm.
+// It was the correct answer!!
 
 CamelNetwork net = new CamelNetwork(File.ReadAllLines("./data_complete.txt"));
-//Console.WriteLine(" > Steps to complete: " + net.FindStepsToReach("AAA", "ZZZ")); //p1
-Console.WriteLine(" > Steps to complete: " + net.FindStepsToReachGhostMode()); //p2
+Console.WriteLine("LCM: " + net.FindStepsToReachGhostMode());
