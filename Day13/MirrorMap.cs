a4104a3e31a83a2f2770cfaa901b5b8ccd0c507a
Jesper F <jesperfagerberg@live.se>
2023-12-21 20:32:05 +0100

    Day 13 part 1 complete

---
+++ Day13/MirrorMap.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Day13.MirrorMap;
+
+namespace Day13
+{
+
+    public class MirrorMap
+    {
+        public readonly Terrain[,] mirrors; // true == rocks (#), false = ash (.)
+        public readonly Terrain[][] rows;
+        public readonly Terrain[][] columns;
+
+        public enum Terrain
+        {
+            Ash = 0,
+            Rocks = 1
+        }
+
+
+        public MirrorMap(Terrain[,] mirrors)
+        {
+            this.mirrors = mirrors;
+        }
+        public MirrorMap(string[] rowInput)
+        {
+            mirrors = new Terrain[rowInput[0].Length, rowInput.Length];
+            rows = new Terrain[mirrors.GetLength(1)][];    // getlength(1) is the length of all nr of x values
+            columns = new Terrain[mirrors.GetLength(0)][]; // getlength(0) is the length of all nr of y values
+
+            for (int i = 0; i < mirrors.GetLength(1); i++)
+            {
+                for (int j = 0; j < mirrors.GetLength(0); j++)
+                {
+                    mirrors[j, i] = (rowInput[i][j] == '#') ? Terrain.Rocks : Terrain.Ash;
+                }
+            }
+
+            for (int i = 0; i < mirrors.GetLength(1); i++)
+            {
+                Terrain[] tarray = new Terrain[mirrors.GetLength(0)];
+                for (int j = 0; j < mirrors.GetLength(0); j++)
+                {
+                    tarray[j] = mirrors[j, i];
+                }
+                rows[i] = tarray;
+            }
+
+            for (int i = 0; i < mirrors.GetLength(0); i++)
+            {
+                Terrain[] tarray = new Terrain[mirrors.GetLength(1)];
+                for (int j = 0; j < mirrors.GetLength(1); j++)
+                {
+                    tarray[j] = mirrors[i, j];
+                }
+                columns[i] = tarray;
+            }
+
+
+        }
+
+        public long Solve()
+        {
+            long value = 0;
+
+            // If mirrored along y (rows)
+            long rowValue = 0;
+            for (int i = 0; i < rows.Length - 1; i++) // Compares pairs of two
+            {
+                if (rows[i].SequenceEqual(rows[i+1])) // If pairs are equal
+                {
+                    // This clamps the end of the loop such that we don't accidentally step out of range
+                    int length = Math.Min(i + 1, rows.Length - (i + 1));
+
+                    bool wasBroken = false;
+                    // If a pair of two is found, compare the columns to the above and below the pair
+                    for (int j = 1; j < length; j++)
+                    {
+                        // i   -1 is the element to the above the first of the pair.                        Start   ******||****
+                        // i+1 +1 is the element to the below the second of the pair.                       j=1     *****|**|***
+                        // replacing -1/+1 with j leads means we keep comparing until we reach the end      j=2     ****|****|**
+                        if (!rows[i - j].SequenceEqual(rows[i + 1 + j]))
+                        {
+                            wasBroken = true;
+                            break;
+                        }
+                    }
+
+                    // If broken, it means it wasn't a perfect mirror.
+                    if (wasBroken) continue;
+                    else
+                    {
+                        rowValue = 100 * (i + 1);
+                        break;
+                    }
+
+                }
+            }
+            value += rowValue;
+
+            // If mirrored along y (rows)
+            long colValue = 0;
+            for (int i = 0; i < columns.Length - 1; i++) // Compares pairs of two
+            {
+                if (columns[i].SequenceEqual(columns[i + 1])) // If pairs are equal
+                {
+                    // This clamps the end of the loop such that we don't accidentally step out of range
+                    int length = Math.Min(i + 1, columns.Length - (i + 1));
+                    
+                    bool wasBroken = false;
+                    // If a pair of two is found, compare the columns to the right and left of the pair
+                    for (int j = 1; j < length; j++)
+                    {
+                        // i   -1 is the element to the left of the first in the pair.                      Start   ******||****
+                        // i+1 +1 is the element to the right of the second in the pair.                    j=1     *****|**|***
+                        // replacing -1/+1 with j leads means we keep comparing until we reach the end      j=2     ****|****|**
+                        if (!columns[i - j].SequenceEqual(columns[i + 1 + j]))
+                        {
+                            wasBroken = true;
+                            break;
+                        }
+                    }
+
+                    // If broken, it means it wasn't a perfect mirror.
+                    if (wasBroken) continue;
+                    else
+                    {
+                        colValue = (i + 1);
+                        break;
+                    }
+                }
+            }
+            value += colValue;
+
+            return value;
+        }
+
+        public Terrain[] GetRow(int row)
+        {
+            return [.. Enumerable.Range(0, mirrors.GetLength(0))
+                .Select(x => mirrors[x, row])];
+
+            // Explanation:
+            // Enumerable.Range(x,y) generate a list of integers from x to y. (x, x+1, x+2, ..., y-2, y-1)
+            // in .Select() we use each integer from the generated list to select all mirrors[LISTVALUE, row]
+            // We dont need .ToArray() because we already have it in a collection expression (ie [.. collectionOfValues])
+        }
+        public Terrain[] GetColumn(int column)
+        {
+            return [.. Enumerable.Range(0, mirrors.GetLength(1))
+                .Select(y => mirrors[column, y])];
+
+            // Explanation:
+            // Enumerable.Range(x,y) generate a list of integers from x to y. (x, x+1, x+2, ..., y-2, y-1)
+            // in .Select() we use each integer from the generated list to select all mirrors[LISTVALUE, row]
+            // We dont need .ToArray() because we already have it in a collection expression (ie [.. collectionOfValues])
+        }
+
+        public override string? ToString()
+        {
+            // Without numbering
+            string s = "";
+            for (int i = 0; i < mirrors.GetLength(1); i++)
+            {
+                for (int j = 0; j < mirrors.GetLength(0); j++)
+                {
+                    s += mirrors[j, i] == Terrain.Rocks ? '#' : '.';
+                }
+                s += "\n";
+            }
+            return s;
+
+            /*
+            // With numbering. Falls apart when numbering is more than one character long,
+            // for example if you have 10 or more columns
+            string s = "  ";
+            for (int i = 0; i < mirrors.GetLength(0); i++)
+            {
+                s += i;
+            }
+
+            for (int i = 0; i < mirrors.GetLength(1); i++)
+            {
+                s += "\n" + i + " ";
+                for (int j = 0; j < mirrors.GetLength(0); j++)
+                {
+                    s += mirrors[j, i] == Terrain.Rocks ? '#' : '.';
+                }
+            }
+            return s;*/
+        }
+    }
+}
---
+++ Day13/Program.cs
@@ -0,0 +1,54 @@
+﻿
+// Interpret data
+using Day13;
+
+List<MirrorMap> mirrorMaps = new List<MirrorMap>();
+
+string[] rowData = File.ReadAllLines("./data_complete.txt");
+List<string> currentMapAsString = new List<string>();
+int rowCounter = 0;
+int mapNr = 1;
+long totalValue = 0;
+while (rowCounter < rowData.Length)
+{
+    if (rowData[rowCounter].Length > 1)
+        currentMapAsString.Add(rowData[rowCounter++]);
+    
+    if (rowData[rowCounter].Length == 0 || rowCounter == rowData.Length - 1)
+    {
+        MirrorMap mm = new([.. currentMapAsString]);
+        mirrorMaps.Add(mm);
+        currentMapAsString.Clear();
+        Console.WriteLine(mm.ToString());
+        rowCounter++;
+
+        long value = mm.Solve();
+        totalValue += value;
+
+        Console.WriteLine("Map " + mapNr);
+        Console.WriteLine("| Value: " + value);
+        Console.WriteLine("| Total: " + totalValue + "\n\n");
+        mapNr++;
+
+        //MirrorMap.Terrain[] row = mm.GetRow(0);
+        //MirrorMap.Terrain[] column = mm.GetColumn(0);
+        //Console.WriteLine("Row: " + string.Join(' ', row.Select(x => x == MirrorMap.Terrain.Rocks ? '#' : '.')));
+        //Console.WriteLine("Col: " + string.Join(' ', column.Select(x => x == MirrorMap.Terrain.Rocks ? '#' : '.')));
+    }
+}
+
+
+
+/*string[] colData = new string[rowData[0].Length];
+for (int i = 0; i < colData.Length; i++)
+{
+    string col = "";
+    for (int j = 0; j < rowData[i].Length; j++)
+    {
+        col += rowData[i][j];
+    }
+    colData[i] = col;
+    Console.WriteLine(col);
+}*/
+
+
