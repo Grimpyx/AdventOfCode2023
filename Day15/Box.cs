36d8313b83cbb7b39bd38fcb7bac109eb3b211d1
Jesper F <jesperfagerberg@live.se>
2023-12-26 00:33:46 +0100

    Day 15 complete

---
+++ Day15/Box.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15
+{
+    public class Box
+    {
+        //private int lenses = 0b0000_0000;
+        //List<Lens> lenses = new List<Lens>();
+
+        //Dictionary<string, byte> lenses = new Dictionary<string, byte>(); // string label,  byte focalLength
+
+        public List<Lens> Lenses { get; private set; }
+
+        public Box()
+        {
+            Lenses = [];
+        }
+
+        //public int Lenses { get => lenses; private set => lenses = value; }
+
+        /*public void SetLens(byte lensNr, bool value)
+        {
+            if (lensNr < 1 || lensNr > 9) throw new IndexOutOfRangeException();
+
+            if (value)
+                Lenses |= 1 << (lensNr - 1);
+            else
+                Lenses &= ~(1 << (lensNr - 1));
+        }*/
+
+        public void AddLens(string label, byte focalLength)
+        {
+            string[] allLabels = [.. Lenses.Select(l => l.label)];
+
+            if (allLabels.Contains(label)) // remove old one and insert new one at the same spot
+            {
+                int index = Lenses.IndexOf(Lenses.Where(l => l.label == label).First());
+                Lenses[index].label = label;
+                Lenses[index].focalLength = focalLength;
+                //Lenses.RemoveAt(index);
+                //Lenses.Insert(index, new Lens(label, focalLength));
+            }
+            else
+            {
+                Lenses.Add(new Lens(label, focalLength));
+            }
+
+
+            // Case 1, lens already exists so we replace it
+            /*string[] matches = [.. lenses.Keys.Where(x => x == label)];
+            if (matches )*/
+        }
+
+        public void RemoveLens(string label)
+        {
+            var matches = Lenses.Where(x => x.label == label).ToArray();
+            if (!matches.Any()) return;
+
+            int index = Lenses.IndexOf(matches[0]);
+            //Lenses.RemoveAll(x => x.label == label);
+            //Lenses[index] = Lens.Empty;
+            Lenses.RemoveAt(index);
+
+            //lenses = [.. lenses.OrderBy(x => x.focalLength)];
+
+            /*if (!lenses.ContainsKey(label)) return;
+
+            byte pos = lenses[label];
+
+            lenses.Remove(label);
+            //            lenses = [.. lenses.OrderBy(x => x.focalLength)];
+            foreach (var key in lenses.Keys)
+            {
+                if (lenses[key] > pos) lenses[key]--;
+            }*/
+
+
+
+            /*for (byte i = 0; i < lenses.Count; i++)
+            {
+                lenses[i].focalLength = (byte)(i + 1); // move all focal lengths forward, range 1-9
+            }*/
+        }
+    }
+
+    public class Lens
+    {
+        public string label;
+        public byte focalLength;
+        public bool IsEmpty { get; private set; }
+
+        public static Lens Empty = new Lens();
+
+        public Lens(string label, byte focalLength)
+        {
+            this.label = label;
+            this.focalLength = focalLength;
+        }
+
+        private Lens() => IsEmpty = true;
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Lens l) return false;
+            return (l.label == label) && (l.focalLength == focalLength) && (l.IsEmpty == IsEmpty);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(label, focalLength);
+        }
+
+        public override string? ToString()
+        {
+            return $"(Label:{label}, Focal:{focalLength})";
+        }
+    }
+}
---
+++ Day15/Program.cs
@@ -0,0 +1,106 @@
+﻿
+using Day15;
+
+
+string data = File.ReadAllText("./data_complete.txt");
+//string data = File.ReadAllText("./data_test.txt");
+string[] splitData = data.Split(',');
+long sumOfResults = 0;
+
+
+//Part1();
+Part2();
+
+void Part1()
+{
+    foreach (string s in splitData)
+    {
+        if (s == string.Empty) continue;
+        if (s == "\r\n") continue;
+        PrintHashFor(s.Trim(['\r', '\n']));
+    }
+    Console.WriteLine(" > Result: " + sumOfResults);
+
+    void PrintHashFor(string s)
+    {
+        int hashFromString = HashAlgorithm(s);
+        Console.WriteLine($"\"{s}\" has hash {hashFromString}.");
+        sumOfResults += hashFromString;
+    }
+}
+
+void Part2()
+{
+    // Create boxes
+    Dictionary<byte, Box> boxes = new();
+    for (int i = 0; i < byte.MaxValue + 1; i++)
+    {
+        boxes.Add((byte)i, new Box());
+    }
+
+    // Add/remove lenses depending on the input
+    foreach (string s in splitData)
+    {
+        string lensLabel;
+        byte boxNr;
+
+        // in case we have an '='
+        if (char.IsDigit(s[^1]))
+        {
+            byte focalLength = byte.Parse(s[^1..]);
+            lensLabel = s[..^2];
+            boxNr = HashAlgorithm(lensLabel);
+
+            boxes[boxNr].AddLens(lensLabel, focalLength);
+        }
+        else // If operation is '-'
+        {
+            lensLabel = s[..^1];
+            boxNr = HashAlgorithm(lensLabel);
+
+            boxes[boxNr].RemoveLens(lensLabel);
+        }
+    }
+
+    long focusingPower = 0;
+    // Calculate focusing power
+    for (int boxIndex = 0; boxIndex < 256; boxIndex++)
+    {
+        Box box = boxes[(byte)boxIndex];
+        // For each lens
+        // + focal length
+        int lensCounter = 0;
+        foreach (Lens l in box.Lenses)
+        {
+            if (l.IsEmpty) continue;
+            lensCounter++; // start counting from 1, not 0
+
+            long fpBox = 1 + boxIndex; // boxIndex is 0-255, but we need it to be 1-256
+            fpBox *= lensCounter * l.focalLength; // as per the instruction
+
+            Console.WriteLine("fp for lens " + l.label + ": " + fpBox);
+            focusingPower += fpBox;
+        }
+
+    }
+    Console.WriteLine(" > Tot: " + focusingPower);
+}
+
+
+// Per instruction
+byte HashAlgorithm(string s)
+{
+    if (s == string.Empty) return 0;
+    if (s == "\r\n") return 0;
+
+    int number = 0;
+    foreach (char c in s)
+    {
+        if (c == '\n' || c == '\0' || c == '\r') continue;
+        number += c;
+        number *= 17;
+        number %= 256;
+    }
+
+    return (byte)number;
+}
