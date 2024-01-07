5f33f5c9e6863b5f2a1517b7c4bee36346d0dd67
Jesper F <jesperfagerberg@live.se>
2024-01-07 18:23:35 +0100

    Day 20 p1 complete

---
+++ Day20/Module.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20
+{
+    abstract class Module
+    {
+        public static int totalHighPulses = 0;
+        public static int totalLowPulses = 0;
+
+        public string name;
+        private int queueWeight = 0;
+
+        public static Dictionary<string, Module> moduleDict = new Dictionary<string, Module>();
+
+        public PriorityQueue<(Pulse pulse, Module sender), int> receiveQueue;
+
+        public List<string> destinationModules = new List<string>();
+
+        protected Module(string name, List<string> destinationModules)
+        {
+            this.name = name;
+            this.destinationModules = destinationModules;
+            moduleDict.Add(name, this);
+
+            receiveQueue = new PriorityQueue<(Pulse, Module), int>();
+        }
+
+        protected abstract void ReceivePulse(Pulse p, Module sender);
+
+
+        protected virtual void BroadcastPulse(Pulse p)
+        {
+            foreach (var item in destinationModules)
+            {
+                Module m = GetOrCreateModule(item);
+                m.QueueReceive(p, this);
+                //item.ReceivePulse(p, this);
+                /*if (p == Pulse.High)
+                    ;
+                else if (p == Pulse.Low)
+                    ;*/
+            }
+        }
+
+        protected Module GetOrCreateModule(string moduleName)
+        {
+            if (moduleDict.TryGetValue(moduleName, out Module m))
+            {
+                return m;
+            }
+            else
+            {
+                Module newm = new OutputModule(moduleName, []);
+                return newm;
+            }
+        }
+
+        public void QueueReceive(Pulse p, Module sender)
+        {
+            receiveQueue.Enqueue((p,sender), queueWeight);
+            queueWeight++;
+
+            switch (p) 
+            {
+                case Pulse.High:
+                    totalHighPulses++;
+                    break;
+                case Pulse.Low:
+                    totalLowPulses++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Step(bool writeResult)
+        {
+            var inst = receiveQueue.Dequeue();
+            if (writeResult)
+            {
+                string pulse = inst.pulse == Pulse.High ? "-high" : "-low";
+                Console.WriteLine($"{inst.sender} {pulse} -> {name}");
+            }
+            ReceivePulse(inst.pulse, inst.sender);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Module module &&
+                   name == module.name;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(name);
+        }
+
+        public override string? ToString()
+        {
+            return name;
+        }
+
+        public enum Pulse
+        {
+            High,
+            Low
+        }
+
+        public enum Instruction
+        {
+            ReceivePulse,
+            BroadcastPulse
+        }
+    }
+
+    class FlipFlopModule : Module
+    {
+        bool flipFlopState;
+
+        public FlipFlopModule(string name, List<string> destinationModules) : base(name, destinationModules)
+        {
+            this.flipFlopState = false;
+        }
+
+        protected override void ReceivePulse(Pulse p, Module sender)
+        {
+            //base.ReceivePulse(p);
+            if (p == Pulse.Low)
+            {
+                flipFlopState = !flipFlopState;
+
+                if (flipFlopState == true)
+                    BroadcastPulse(Pulse.High);
+                else if (flipFlopState == false)
+                    BroadcastPulse(Pulse.Low);
+            }
+        }
+    }
+
+    class ConjunctionModule : Module
+    {
+        public Dictionary<Module, Pulse> rememberedPulses;
+
+        public ConjunctionModule(string name, List<string> destinationModules) : base(name, destinationModules)
+        {
+            rememberedPulses = new Dictionary<Module, Pulse>();
+            /*foreach (var item in moduleDict.Values)
+            {
+                if (item.name != "broadcaster" && item != this)
+                    rememberedPulses.Add(item, Pulse.Low);
+            }*/
+        }
+
+        public void InitializeRememberedPulses()
+        {
+            // Find all connections to This and add a reference to it
+            var allModules = moduleDict.Values;
+            foreach (var item in allModules.ToArray())
+            {
+                foreach (var s in item.destinationModules)
+                {
+                    Module m = GetOrCreateModule(s);
+                    if (m == this)
+                        rememberedPulses[item] = Pulse.Low;
+                }
+            }
+        }
+
+        protected override void ReceivePulse(Pulse p, Module sender)
+        {
+            rememberedPulses[sender] = p;
+
+            bool allIsHigh = true;
+            foreach (var item in rememberedPulses)
+            {
+                if (item.Value == Pulse.Low) allIsHigh &= false;
+            }
+
+            if (allIsHigh) BroadcastPulse(Pulse.Low);
+            else BroadcastPulse(Pulse.High);
+        }
+
+        /*protected override void BroadcastPulse(Pulse p)
+        {
+            foreach (var item in destinationModules)
+            {
+                //Module m = moduleDict[item];
+                //Pulse rememberedPulse = rememberedPulses[m];
+
+                Module m = moduleDict[item];
+                m.QueueReceive(p, this);
+            }
+        }*/
+    }
+
+    class BroadcastModule : Module
+    {
+        public BroadcastModule(string name, List<string> destinationModules) : base(name, destinationModules)
+        {
+
+        }
+
+        protected override void ReceivePulse(Pulse p, Module sender)
+        {
+            BroadcastPulse(p);
+        }
+    }
+
+    class ButtonModule : Module
+    {
+        Module? broadcaster;
+
+        public ButtonModule(string name, List<string> destinationModules) : base(name, destinationModules)
+        {
+            foreach (var item in destinationModules)
+            {
+                Module m = moduleDict[item];
+                if (m is BroadcastModule)
+                {
+                    broadcaster = m;
+                    break;
+                }
+            }
+        }
+
+        public void PushButton()
+        {
+            BroadcastPulse(Pulse.Low);
+        }
+
+        protected override void ReceivePulse(Pulse p, Module sender)
+        {
+            Console.WriteLine("Button received a pulse.");
+        }
+
+        protected override void BroadcastPulse(Pulse p)
+        {
+            if (broadcaster != null) broadcaster.QueueReceive(p, this);
+            else Console.WriteLine("Tried to press button but failed.");
+        }
+    }
+
+    class OutputModule : Module
+    {
+        public OutputModule(string name, List<string> destinationModules) : base(name, destinationModules)
+        {
+        }
+
+        protected override void ReceivePulse(Pulse p, Module sender)
+        {
+
+        }
+
+        protected override void BroadcastPulse(Pulse p)
+        {
+
+        }
+    }
+}
---
+++ Day20/Program.cs
@@ -0,0 +1,99 @@
+﻿using Day20;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+//List<Module> modules = InterpretData(File.ReadAllLines("./data_test.txt"));
+List<Module> modules = InterpretData(File.ReadAllLines("./data_complete.txt"));
+
+// Part 1 7307975XX correct
+
+
+void Part1()
+{
+    Stopwatch sw = Stopwatch.StartNew();
+    bool writeOutput = true;
+
+    ButtonModule button = new ButtonModule("button", ["broadcaster"]);
+    int buttonPresses = 1000;
+    for (int numberOfButtonPresses = 0; numberOfButtonPresses < buttonPresses; numberOfButtonPresses++)
+    {
+        if (writeOutput) Console.WriteLine($"\nPressed button (step {numberOfButtonPresses + 1})");
+        button.PushButton();
+
+        while (!AdvanceQueues(writeOutput))
+        {
+            //Thread.Sleep(100);
+            //Console.WriteLine();
+        }
+    }
+    sw.Stop();
+    Console.WriteLine(" > Total high: " + Module.totalHighPulses);
+    Console.WriteLine(" > Total  low: " + Module.totalLowPulses);
+    Console.WriteLine(" > Multiplied: " + (Module.totalLowPulses * Module.totalHighPulses));
+
+    Console.WriteLine("\nDone! Took " + sw.ElapsedMilliseconds + "ms");
+}
+
+bool AdvanceQueues(bool writeResult) // returns True if complete
+{
+    //modules.ForEach(module => module.Step());
+    bool noUpdate = true;
+    List<Module> modulesToStep = new List<Module>();
+    foreach (var m in Module.moduleDict.Values)
+    {
+        if (m.receiveQueue.Count > 0)
+        {
+            modulesToStep.Add(m);
+            noUpdate &= false;
+        }
+    }
+    modulesToStep.ForEach(module => module.Step(writeResult));
+    if (writeResult) Console.WriteLine();
+    return noUpdate;
+}
+
+
+// Interprets the data by creating modules from each line.
+List<Module> InterpretData(string[] ReadAllLines)
+{
+    List<Module> list = new List<Module>();
+    List<ConjunctionModule> conjunctionModules = new List<ConjunctionModule>(); // this exists to use later for a specific initialization
+
+    for (int i = 0; i < ReadAllLines.Length; i++)
+    {
+        string line = ReadAllLines[i];
+
+        string leftS = line.Split(" -> ")[0];
+        string rightS = line.Split(" -> ")[1];
+
+        string name = "";
+        List<string> destinations = [..rightS.Split(", ")];
+
+        switch (leftS[0])
+        {
+            case '%':
+                name = leftS[1..];
+                list.Add(new FlipFlopModule(name, destinations));
+                break;
+            case '&':
+                name = leftS[1..];
+                ConjunctionModule cm = new ConjunctionModule(name, destinations);
+                conjunctionModules.Add(cm);
+                list.Add(cm);
+                break;
+            default: // broadcaster
+                name = leftS;
+                list.Add(new BroadcastModule(name, destinations));
+                break;
+        }
+    }
+
+    // Initialize remembered pulses for all conjunction modules.
+    // This needs to be done after all modules are created to avoid null references.
+    foreach (ConjunctionModule item in conjunctionModules)
+    {
+        item.InitializeRememberedPulses();
+    }
+
+    return list;
+}
