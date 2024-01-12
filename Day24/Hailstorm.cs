5700c6fab5e8a699b459e1e6b97d09dc84dcd33a
Jesper F <jesperfagerberg@live.se>
2024-01-12 15:30:11 +0100

    Day 24 complete

---
+++ Day24/Hailstorm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day24
+{
+    class Hailstorm
+    {
+        private HashSet<Hail3D> hailHashset = new HashSet<Hail3D>();
+
+        public Hailstorm(HashSet<Hail3D> hail)
+        {
+            this.hailHashset = hail;
+        }
+
+        public HashSet<Hail3D> HailHashset { get => hailHashset; }
+
+        public Dictionary<Hail3D, List<(Hail3D other, (decimal x, decimal y) intersectionPoint, bool isInsideTestArea)>> GetIntersectionsXY(Vector2 testAreaMinMax)
+        {
+            // The result to be returned by this method
+            Dictionary<Hail3D, List<(Hail3D other, (decimal x, decimal y) intersectionPoint, bool isInsideTestArea)>> result = [];
+
+            // Because we can't loop through a hashset with indices, so we make an array of the elements
+            Hail3D[] hs = [.. hailHashset];
+
+            for (int i = 0; i < hs.Length; i++)
+            {
+                Hail3D thisHail = hs[i];
+
+                // We start counting from i+1 so we dont compare with any previously checked, because a.Intersect(b) == b.Intersect(a)
+                for (int j = i + 1; j < hs.Length; j++)
+                {
+                    Hail3D otherHail = hs[j];
+
+                    if (thisHail.IntersectsPathWithXYWithinArea(otherHail, testAreaMinMax,
+                        out (decimal x, decimal y) ip, out bool isInside, out _))
+                    {
+                        if (!result.ContainsKey(thisHail))
+                            result.Add(thisHail, []);
+
+                        result[thisHail].Add((otherHail, ip, isInside));
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    class Hail3D(Decimal3 position, Decimal3 velocity)
+    {
+        private Decimal3 position = position; //  x  y  z
+        private Decimal3 velocity = velocity; // kx ky kz
+
+        public Decimal3 Velocity { get => velocity; }
+        public Decimal3 Position { get => position; }
+
+
+        public bool IntersectsPathWithXYWithinArea(Hail3D otherHail, Vector2 testAreaMinMax, out (decimal x, decimal y) intersectionPoint, out bool isInsideTestArea, out decimal intersectionTime)
+        {
+            isInsideTestArea = false;
+            intersectionPoint = default;
+            intersectionTime = -1;
+
+            if (!IntersectsPathWithXY(otherHail, out (decimal x, decimal y) ixy, out decimal time)) return false;
+            intersectionTime = time;
+            intersectionPoint = ixy;
+
+            /* Unnecessary check
+            var ty1 = (y - (decimal)thisHail.position.Y) / (decimal)thisHail.velocity.Y;   // vector from start to intersection point, divide by direction to get >1 if it follows the direction, <1 if it doesnt
+            var ty2 = (y - (decimal)otherHail.position.Y) / (decimal)otherHail.velocity.Y;
+            if (ty1 < 0 || ty2 < 0)
+                return false;*/
+
+            // Check if inside test area
+            if (ixy.x >= (decimal)testAreaMinMax.X && ixy.x <= (decimal)testAreaMinMax.Y
+                && ixy.y >= (decimal)testAreaMinMax.X && ixy.y <= (decimal)testAreaMinMax.Y)
+                isInsideTestArea = true;
+
+            return true;
+        }
+
+        public bool IntersectsPathWithXY_velocityOffset(Hail3D otherHail, (decimal dvx, decimal dvy) dv, out (decimal x, decimal y) intersectionPoint, out decimal intersectionTime)
+        {
+            Hail3D a = new(position, velocity + new Decimal3(dv.dvx, dv.dvy, 0));
+            Hail3D b = new(otherHail.Position, otherHail.Velocity + new Decimal3(dv.dvx, dv.dvy, 0));
+
+            intersectionPoint = default;
+            intersectionTime = -1;
+            if (!a.IntersectsPathWithXY(b, out var ip, out decimal iTime)) return false;
+            intersectionPoint = ip;
+            intersectionTime = iTime;
+            return true;
+        }
+
+        public bool IntersectsPathWithXY(Hail3D otherHail, out (decimal x, decimal y) intersectionPoint, out decimal intersectionTime)
+        {
+            Hail3D thisHail = this;
+            intersectionPoint = default;
+            intersectionTime = -1;
+
+            // if we have two lines
+            // y-y1=k1(x-x1)
+            // y-y2=k2(x-x2)
+            // we can calculate the x-value where they intersect by subtracting the left sides and right sides respectively.
+            // (y-y1)-(y-y2) = (k1(x-x1))-(k2(x-x2))
+            // Simplifying leaves us with
+            // x = (y1-y2+k2*x2-k1*x1) / (k2-k1)
+            // If this value "x" exists, then they intersect.
+            decimal x1 = thisHail.position.X;
+            decimal y1 = thisHail.position.Y;
+            decimal x2 = otherHail.position.X;
+            decimal y2 = otherHail.position.Y;
+
+            if (thisHail.velocity.X == 0) return false;
+            if (otherHail.velocity.X == 0) return false;
+
+            // k represents how many steps y increases per step x
+            decimal k1 = thisHail.velocity.Y / thisHail.velocity.X;
+            decimal k2 = otherHail.velocity.Y / otherHail.velocity.X;
+
+            // k1 can not be equal to k2, else our formula divides by zero
+            if (k1 == k2) return false;
+
+            // Our formula
+            decimal x = (y1 - y2 + (k2 * x2) - (k1 * x1)) / (k2 - k1);
+
+            // If we insert this x into any of the lines we get the y value for the intersection
+            decimal y = y1 + (k1 * x) - (k1 * x1);
+
+            // Check if intersection is back in time, if so then return false
+            // y = y0 + t*vy
+            // ( y - y0 ) / vy = t
+            decimal tx1 = (x - x1);
+            tx1 /= thisHail.velocity.X;
+            decimal tx2 = (x - x2);
+            tx2 /= otherHail.velocity.X;
+
+            //decimal tx1 = (x - thisHail.position.X) / thisHail.velocity.X;   // vector from start to intersection point, divide by direction to get >1 if it follows the direction, <1 if it doesnt
+            //decimal tx2 = (x - otherHail.position.X) / otherHail.velocity.X;
+            //if (tx1 < 0 || tx2 < 0)
+            if (tx1 < 0 || tx2 < 0)
+                return false;
+
+            intersectionPoint = (Math.Round(x, 3), Math.Round(y, 3));
+            intersectionTime = Math.Round(tx2, 3);
+
+            return true;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Hail3D d &&
+                   position.Equals(d.position) &&
+                   velocity.Equals(d.velocity);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(position, velocity);
+        }
+
+        public override string? ToString()
+        {
+            return $"(X:{position.X:e}, Y:{position.Y:e}, Z:{position.Z:e})";
+        }
+    }
+
+    record Decimal3(decimal X, decimal Y, decimal Z)
+    {
+        public static Decimal3 operator +(Decimal3 a, Decimal3 b) => new(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+    }
+}
---
+++ Day24/Program.cs
@@ -0,0 +1,148 @@
+﻿
+using Day24;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.Intrinsics.X86;
+
+bool useCompleteData = true;
+
+string testDataPath = "./data_test.txt";
+string completeDataPath = "./data_complete.txt";
+
+//Part1(); // 16470 too high
+Part2();
+
+void Part2()
+{
+    // Because we essentially have 300 trajectories already defined
+    // we *should* be able to intersect all of them as long as we have
+    // all unknowns calculated.
+    // x = x_0 + t*a
+    // y = y_0 + t*b
+    // z = z_0 + t*c
+    // or for three lines 1, 2, and 3:
+    // PI1 = P1 + t*V1
+    // PI2 = P2 + t*V2
+    // PI3 = P3 + t*V3
+    // pI1..pI3 all on the same line, i.e. has same equation
+    // There exists a value t such that
+    // PI1 = P1 + t*V1
+    // PI2 = P2 + t*V2
+    // ...
+    // PIn = Pn + t*Vn
+    // where PI1, PI2, ... PIn all exist on the same line.
+    // I can't really wrap my head around solving this mathematically through code.
+
+    // Big thank you to Ryan Heath who I am shamelessly plagiarizing here https://github.com/ryanheath/aoc2023
+    // (Although I changed it such that it works for my existing systems, with added comments)
+    // Everything else used in this method is my own creation!
+
+    Hailstorm storm;
+    if (useCompleteData) storm = InterpretData(completeDataPath);
+    else storm = InterpretData(testDataPath);
+
+    Hail3D[] hails = storm.HailHashset.OrderBy(x => x.Position.X).ToArray();
+
+    var (hail0, hail1, hail2, hail3) = (hails[0], hails[1], hails[^2], hails[^1]);
+
+    foreach (var dvy in SearchSpace())
+    {
+        foreach (var dvx in SearchSpace())
+        {
+            // If we find an offset dvx dvy (var y and var x in search space)
+            // that leads to the first line intersecting with all other three (second, and last, and second last)
+            (decimal x, decimal y) ip01;
+            decimal t01;
+            (decimal x, decimal y) ip02;
+            decimal t02;
+            (decimal x, decimal y) ip03;
+            decimal t03;
+
+            // IsIntersectionXY checks intersection between lines 1 and 2 with an offset velocity, ignoring Z coordinate
+            if (!hail0.IntersectsPathWithXY_velocityOffset(hail1, (dvx, dvy), out ip01, out t01))
+                continue;
+            if (!hail0.IntersectsPathWithXY_velocityOffset(hail2, (dvx, dvy), out ip02, out t02))
+                continue;
+            if (!hail0.IntersectsPathWithXY_velocityOffset(hail3, (dvx, dvy), out ip03, out t03))
+                continue;
+
+            // If the intersection coordinates aren't the same
+            //if ((ip01.y, ip01.x) != (ip02.y, ip02.x) || (hail1.y, hail1.x) != (ip03.y, ip03.x)) continue;
+            if (ip01 != ip02 || ip01 != ip03)
+                continue;
+
+            // if all intersects in XY, we can start checking for what Z value
+            // For any value Z
+            foreach (var dvz in SearchSpace())
+            {
+                if (dvz != -249) continue;
+
+                var z1 = Zoft(hail1, t01, dvz);
+                var z2 = Zoft(hail2, t02, dvz);
+                if (z1 != z2)
+                    continue;
+                var z3 = Zoft(hail3, t03, dvz);
+                if (z1 != z3)
+                    continue;
+
+                long result = (long)(ip01.x + ip01.y + z1);
+                Console.WriteLine(" > Done: " + result);
+                return;
+            }
+
+            // Z(t), i.e. a hail's Z position after time t has passed
+            decimal Zoft(Hail3D hail, decimal t, long dvz) => Math.Round(hail.Position.Z + t * (hail.Velocity.Z + dvz), 3);
+        }
+    }
+
+    throw new UnreachableException();
+
+    // Represents the range of deltaV we will explore
+    static IEnumerable<int> SearchSpace() => Enumerable.Range(-300, 600);
+}
+void Part1()
+{
+    Hailstorm storm;
+
+    if (useCompleteData) storm = InterpretData(completeDataPath);
+    else storm = InterpretData(testDataPath);
+
+    Dictionary<Hail3D, List<(Hail3D other, (decimal x, decimal y) intersectionPoint, bool isInsideTestArea)>> result;
+
+    if (useCompleteData)
+        result = storm.GetIntersectionsXY(new Vector2(200000000000000, 400000000000000));
+    else
+        result = storm.GetIntersectionsXY(new Vector2(7, 27));
+
+    List<(Hail3D firstHail, Hail3D secondHail, (decimal x, decimal y) intersectionPoint, bool isInsideTestArea)> totalResults = [];
+    foreach (var list in result)
+    {
+        foreach (var element in list.Value)
+        {
+            totalResults.Add((list.Key, element.other, element.intersectionPoint, element.isInsideTestArea));
+        }
+    }
+
+    Console.WriteLine(" > Total intersecting hail: " + totalResults.Count);
+    Console.WriteLine("             Within bounds: " + totalResults.Where(x => x.isInsideTestArea == true).Count());
+}
+
+Hailstorm InterpretData(string path)
+{
+    string[] data = File.ReadAllLines(path);
+    HashSet<Hail3D> hails = new HashSet<Hail3D>();
+
+    for (int j = 0; j < data.Length; j++)
+    {
+        string[] lines = data[j].Split(" @ ");
+        long[] posArray = lines[0].Split(", ").Select(long.Parse).ToArray();
+        long[] velArray = lines[1].Split(", ").Select(long.Parse).ToArray();
+        //Vector3 pos = new Vector3((float)((double)posArray[0] / 100000000000000d), (float)((double)posArray[1] / 100000000000000d), (float)((double)posArray[2] / 100000000000000d));
+        Decimal3 pos = new Decimal3(posArray[0], posArray[1], posArray[2]);
+        Decimal3 vel = new Decimal3(velArray[0], velArray[1], velArray[2]);
+
+        hails.Add(new Hail3D(pos, vel));
+    }
+    return new Hailstorm(hails);
+}
