04e2859cf782f0d4818f632c793d1b8a287df612
Jesper F <jesperfagerberg@live.se>
2024-01-15 17:17:26 +0100

    Day 21 part 2 complete

--- Day21/Program.cs
+++ Day21/Program.cs
@@ -5,95 +5,91 @@
 using Day21;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.ExceptionServices;
 
 //Part1(); // Answer is 38XX
-Part2();
+Part2(); // Answer is 636350496972143
 void Part2()
 {
-    //int maxSteps = 26501365;
-    int maxSteps = 100;
+    // Had to look at a bunch of visualizations to know what to do here. Thanks AOC reddit!
 
-    // Create first map.
-    MapTile[][] map = InterpretInputP2(File.ReadAllLines("./data_test.txt"));
-    //PrintMap(map, 1, 1);
-    Console.WriteLine();
+    // The steps we need to go is 26501365.
 
-    Map mapObject = new Map(map, MapStartFromDirection(Direction.SW, map));
-    //Map mapObject = new Map(map);
+    // First we create a Stepping algorithm that advances the map, just like in part 1.
+    // But we have to make sure the map it is stepping through is a multiple of the file input.
 
+    // Then we ensure that it works by matching the results (number of gardens we can reach in X steps) to the given answer for the test input.
+    // Steps 6, 10, 50, and 100 gives the correct answers as in the example
+    //
+    //     Step     Gardens reached
+    //      6              16
+    //      10             50
+    //      50            1594
+    //     100            6536
+
+    // Then we have to notice a pattern. The width of each base map is 131 in the complete input.
+    // Essentially, after 65 steps (width/2 - 1 because we start in the MIDDLE of the map) we reach the edges of the first base map, creating a diamond shape.
+    // Then, for every 131st step after that the diamond's edges will have moved to the edges of the next 
+    //  
+    //                                         Step 64+2*131
+    //                      Step 65+131            ..X..
+    //    Step 65               .X.                .XOX.
+    //       X                  XOX                XOOOX
+    //                          .X.                .XOX.
+    //                                             ..X..
+    //
+    // Step 65:       we have reached the edges of the middle base map.
+    // Step 65+131:   we have filled the first middle part, and reached the edge of the next adjacent base maps above/below/left/right.
+    // Step 65+2*131: we have filled all previous base maps and have reached the edges of the next adjacent base maps
+    // and on and on and on and on
+    // 
+    // Because the pattern expands in all directions in two dimensions, this growth is quadratic in nature.
+    // This means that we can make a quadratic regression of three datapoints (required for a second degree polynomial)
+    //
+    // To select the datapoints, we step through the map and save the result of stepping 65+131, 65+2*131, and 65+3*131.
+    // Then we use a regression calculator to calculate the regression, and in that resulting quadratic function we insert 26501365.
+    // !! Make sure the regression calculator has enough precision to display 10^14 numbers !!
+    // (also, if R^2 is not 1, then not all points perfectly align with the regression. Then it will not produce the correct result)
+
+
+    // Create the map.
+    // The input data from the file is called the base map. The complete map is many repeated base maps in all directions.
+    // The assignment says it should repeat to infinity, but for us it is sufficient with a large one, f0r example 111x111
+    //   parameter explanation: width number doesn't really matter that much, as long as it's big enough not to hit any edges
+    (MapTile[][] map, int baseMapWidth) = InterpretInputP2(File.ReadAllLines("./data_complete.txt"), multipliedWidth_MustBeOddNr: 111);
+
+    int maxSteps = 99000; // a large number, doesn't really matter how much
+
+    Map mapObject = new Map(map);
+    List<(int step, int distinct)> distinctList = new(); // This will be used to create a regression
     for (int i = 1; i <= maxSteps; i++)
     {
-        mapObject.Step();
-        if (i == maxSteps)
+        mapObject.Step(); //
+
+        Console.SetCursorPosition(0, 0);
+        Console.WriteLine("Step " + i);
+
+        if (i > baseMapWidth) // every 20th step, record the data
         {
-            Console.SetCursorPosition(0, 0);
-            Console.WriteLine("Step " + i);
-            PrintMap(map, 0, 3);
+            if ((i + (baseMapWidth / 2)) % baseMapWidth == 0) // every 131st step from 65    (65+131, 65+262, 65+393)
+                distinctList.Add((i, mapObject.Distinct));
         }
+        if (distinctList.Count >= 3) break; // If we find our three values, break
+
+        //if (i == maxSteps) PrintMap(map, 0, 3);
     }
-    Console.WriteLine("\nDistinct: " + mapObject.Distinct);
-    Console.ReadLine();
+    Console.WriteLine("\nDistinct: " + mapObject.Distinct + "\n");
 
-    //(MapTile[][] map, (int x, int y) start) = InterpretInput(File.ReadAllLines("./data_complete.txt"));
-    /*
-    Dictionary<Direction, int> filledDirections = new Dictionary<Direction, int>();
-    Direction[] allDirections = [Direction.N, Direction.NE, Direction.E, Direction.SE, Direction.S, Direction.SW, Direction.W, Direction.NW];
+    Console.WriteLine("Coordinates found for the quadratic regression:");
 
-    for (int i = 0; i < allDirections.Length; i++)
+    for (int i = 0; i < distinctList.Count; i++)
     {
-        Direction d = allDirections[i];
-        MapTile[][] tempMap = map;
-        (int x, int y) startCoord = MapCoordFromDirection(d, start, map);
-
-
-        PriorityQueue<(int x, int y)[], int> q1 = new PriorityQueue<(int x, int y)[], int>();
-        q1.Enqueue([startCoord], 0);
-        int currentDistinct = 0;
-        for (int step = 1; ; step++)
-        {
-            // step
-            var nextCoords = q1.Dequeue();
-            List<(int x, int y)> toAdd = new List<(int x, int y)>();
-            
-            bool hitEdge = false;
-            foreach (var coord in nextCoords)
-            {
-                if (GetNeighboursTF(coord, map, out (int x, int y)[] neighs))
-                    toAdd.AddRange(neighs);
-                else
-                {
-                    hitEdge = true;
-                    //PrintMap(map, 0, 2);
-                    break;
-                }
-            }
-            if (hitEdge)
-            {
-                filledDirections.Add(d, currentDistinct);
-                break;
-            }
-
-            // Update map
-            (int x, int y)[] distinct = toAdd.Distinct().ToArray();
-            q1.EnqueueRange([distinct], step);
-            foreach (var item in nextCoords)
-            {
-                tempMap[item.y][item.x] = MapTile.Garden;
-            }
-            foreach (var item in distinct)
-            {
-                tempMap[item.y][item.x] = MapTile.Step;
-            }
-            if (tempMap[start.y][start.x] != MapTile.Step) tempMap[start.y][start.x] = MapTile.Start;
-            
-            currentDistinct = distinct.Length;
-        }
+        var coord = distinctList[i];
+        Console.WriteLine(string.Join(';', [coord.step.ToString(), coord.distinct.ToString()]));
     }
+    Console.WriteLine();
 
-    */
-
-
-
+    /*
     // Find maxtiles for a filled map
     int maxTiles = 0;
     for (int j = 0; j < map.Length; j += 1)
@@ -134,7 +130,7 @@ void Part2()
         }
         else break;
     }
-    Console.WriteLine("Remainder: " + remainder);
+    //Console.WriteLine("Remainder: " + remainder);
 
     //          ^
     //          X
@@ -153,7 +149,7 @@ void Part2()
     // Calculate how many completed maps we will get
 
     // Create maps around it, and loop through them
-
+    */
 
 
 
@@ -195,13 +191,13 @@ void Part2()
         PrintMap(map, 0, 2);
 
         Console.WriteLine($"Total plots available for {step} step(s) is {distinct.Count()}. \n > Press enter to step.");
-    }*/
+    }
 
 
     void AdvanceMaps()
     {
 
-    }
+    }*/
 }
 
 
@@ -331,19 +327,24 @@ bool GetNeighboursTF((int x, int y) coord, MapTile[][] map, out (int x, int y)[]
     return (map, start);
 }
 
-MapTile[][] InterpretInputP2(string[] readAllLines)
+(MapTile[][], int) InterpretInputP2(string[] readAllLines, int multipliedWidth_MustBeOddNr)
 {
+    if (multipliedWidth_MustBeOddNr % 2 != 1)
+    {
+        Console.WriteLine("ERROR! Map width was an even number.");
+        return (new MapTile[0][], -1);
+    }
 
     (MapTile[][] baseMap, (int x, int y) startCoord) = InterpretInputP1(readAllLines);
-    MapTile[][] bigmap = new MapTile[baseMap.Length * 3][];
+    MapTile[][] bigmap = new MapTile[baseMap.Length * multipliedWidth_MustBeOddNr][];
 
 
-    //(int, int) start = (startCoord.x * 3, startCoord.y * 3);
-    for (int j = 0; j < baseMap.Length * 3; j++)
+    //(int, int) start = (startCoord.x * multipliedWidth, startCoord.y * multipliedWidth);
+    for (int j = 0; j < baseMap.Length * multipliedWidth_MustBeOddNr; j++)
     {
         int baseIndexY = j % baseMap.Length;
-        MapTile[] mapRow = new MapTile[baseMap[baseIndexY].Length * 3];
-        for (int i = 0; i < baseMap[baseIndexY].Length * 3; i++)
+        MapTile[] mapRow = new MapTile[baseMap[baseIndexY].Length * multipliedWidth_MustBeOddNr];
+        for (int i = 0; i < baseMap[baseIndexY].Length * multipliedWidth_MustBeOddNr; i++)
         {
             int baseIndexX = i % baseMap[baseIndexY].Length;
 
@@ -353,7 +354,7 @@ MapTile[][] InterpretInputP2(string[] readAllLines)
         bigmap[j] = mapRow;
     }
 
-    return bigmap;
+    return (bigmap, baseMap.Length);
 }
 
 void PrintMap(MapTile[][] map, int x, int y)
